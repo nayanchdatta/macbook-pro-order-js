5eef25261821b8c13395adbbd1bd3d519ba02956
Nayan Chandra Datta <im.nayanchdatta@gmail.com>
2021-12-23 02:02:47 +0600

    Optimized js code

--- main.js
+++ main.js
@@ -1,83 +1,58 @@
+// Default value for products
 const memory8gb = 0, memory16gb = 180;
 const storage256gb = 0, storage512gb = 100, storage1tb = 180;
 const deliveryPrime = 0, deliveryNormal = 20;
 
-// default value or value initaialize 
+// value initaialize 
 let extraMemoryCost = 0, extraStorageCost = 0, totaldDeliveryCost = 0;
 let totalProductCost = 1299;
 
-// Add event listener to Memory buttons
-document.getElementById('8gb-memory').addEventListener('click', function (){
-    extraMemory = document.getElementById('extra-memory');
-    extraMemory.innerText = memory8gb;
+// optimized Code 
+document.getElementById('right-div').addEventListener('click', function (event){
+    const extraMemory = document.getElementById('extra-memory');
+    const extraStorage = document.getElementById('extra-storage');
+    const deliveryCharge = document.getElementById('delivery-charge');
 
-    extraMemoryCost = memoryCost(memory8gb);
+
+    const eventId = event.target.id;
+
+    switch(eventId){
+        case '8gb-memory':
+            extraMemory.innerText = memory8gb;
+            extraMemoryCost = memory8gb;
+            break;
+        case '16gb-memory':
+            extraMemory.innerText = memory16gb;
+            extraMemoryCost = memory16gb;
+            break;
+        case '256gb-ssd':
+            extraStorage.innerText = storage256gb;
+            extraStorageCost = storage256gb;
+            break;
+        case '512gb-ssd':
+            extraStorage.innerText = storage512gb;
+            extraStorageCost = storage512gb;
+            break;
+        case '1tb-ssd':
+            extraStorage.innerText = storage1tb;
+            extraStorageCost = storage1tb;
+            break;
+        case 'prime-delivery':
+            deliveryCharge.innerText = deliveryPrime;
+            totaldDeliveryCost = deliveryPrime;
+            break;
+        case 'normal-delivery':
+            deliveryCharge.innerText = deliveryNormal;
+            totaldDeliveryCost = deliveryNormal;
+            break;
+        case 'apply-btn':
+            applyPromo();
+            break;
+    }
+
     totalCost();
+
 });
-document.getElementById('16gb-memory').addEventListener('click', function (){
-    extraMemory = document.getElementById('extra-memory');
-    extraMemory.innerText = memory16gb;
-
-    extraMemoryCost = memoryCost(memory16gb);
-    totalCost();
-});
-
-
-// Add event listener to storage buttons
-document.getElementById('256gb-ssd').addEventListener('click', function (){
-    extraStorage = document.getElementById('extra-storage');
-    extraStorage.innerText = storage256gb;
-
-    extraStorageCost = storageCost(storage256gb);
-    totalCost();
-});
-document.getElementById('512gb-ssd').addEventListener('click', function (){
-    extraStorage = document.getElementById('extra-storage');
-    extraStorage.innerText = storage512gb;
-
-    extraStorageCost = storageCost(storage512gb);
-    totalCost();
-});
-document.getElementById('1tb-ssd').addEventListener('click', function (){
-    extraStorage = document.getElementById('extra-storage');
-    extraStorage.innerText = storage1tb;
-
-    extraStorageCost = storageCost(storage1tb);
-    totalCost();
-});
-
-
-// Add event listener to delivery buttons
-document.getElementById('prime-delivery').addEventListener('click', function (){
-    deliveryCharge = document.getElementById('delivery-charge');
-    deliveryCharge.innerText = deliveryPrime;
-
-    totaldDeliveryCost = deliveryCost(deliveryPrime);
-    totalCost();
-});
-document.getElementById('normal-delivery').addEventListener('click', function (){
-    deliveryCharge = document.getElementById('delivery-charge');
-    deliveryCharge.innerText = deliveryNormal;
-
-    totaldDeliveryCost = deliveryCost(deliveryNormal);
-    totalCost();
-});
-
-// get costs
-function memoryCost(extraMemoryCost){
-    // console.log(extraMemoryCost);
-    return extraMemoryCost;
-}
-function storageCost(extraStorageCost){
-    // console.log(extraStorageCost);
-    return extraStorageCost;
-}
-function deliveryCost(extraDeliveryCost){
-    // console.log(extraDeliveryCost);
-    return extraDeliveryCost;
-}
-
-
 
 // Total product cost 
 function totalCost(){
@@ -88,16 +63,8 @@ function totalCost(){
     totalProductCost  = bestPrice + extraMemoryCost + extraStorageCost + totaldDeliveryCost;
     // console.log(totalProductCost);
 
-    let totalProductPrice = document.getElementById('total-p-price');
-    totalProductPrice.innerText = totalProductCost;
+    document.getElementById('total-p-price').innerText = totalProductCost;
 }
-
-
-
-// Appy promo code 
-document.getElementById('apply-btn').addEventListener('click', function (){
-    applyPromo();
-});
 
 
 // Total product cost after promo applied
@@ -114,5 +81,3 @@ function applyPromo(){
         // console.log(totalPrice);
     }
 }
-
-
